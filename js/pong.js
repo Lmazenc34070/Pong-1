32c1ce62de3beacebab126043d2ebdb1567f57a3
DavidBorie <72392653+DavidBorie@users.noreply.github.com>
2020-11-06 16:40:16 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -1,11 +1,11 @@
 let largeur=$("#balle").width();
 let gauche=parseInt($("#balle").css("left"));
 let haut=parseInt($("#balle").css("top"));
-alert(largeur);
+alert(gauche);
 
 setInterval(function(){
     gauche=gauche+1;
     haut=haut+0.5
     $("#balle").css("left",gauche)
-    $("#balle").css("trop");
-}, 10);''
+    $("#balle").css("top", haut);
+}, 10);
