b94a2d6fe4d04f4c5800e17a037eaa5ed80acb12
MAZENC Lucas <72392592+Lmazenc34070@users.noreply.github.com>
2020-11-06 16:44:39 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -6,6 +6,6 @@ alert(gauche);
 setInterval(function(){
     gauche=gauche+1;
     haut=haut+0.5
-    $("#balle").css("left",gauche)
+    $("#balle").css("left",gauche);
     $("#balle").css("top", haut);
 }, 10);
